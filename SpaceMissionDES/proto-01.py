735ef50138d6bd3646b63304af5497791e2cba9d
Benjamin Merrel <benjamin.merrel@gmail.com>
2022-03-19 22:15:46 -0400

    Implement simulation loop

--- SpaceMissionDES/proto-01.py
+++ SpaceMissionDES/proto-01.py
@@ -1,19 +1,38 @@
 
-from dataclasses import dataclass
+# Standard Library
+from dataclasses import dataclass, field
+from heapq import heappush, heappop
+from pprint import pprint # pretty-printing basic data structures
+
+# Dependencies
 import pandas as pd
 
 #######################################################################################################################
-# Events
+# Simulation clock -- taken from Prof. Vuduc's example code
+
+# https://stackoverflow.com/questions/279561/what-is-the-python-equivalent-of-static-variables-inside-a-function
+def static_vars(**kwargs):
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
+
+@static_vars(t=0)
+def now():
+    return now.t
+
+def set_time(t_new=0):
+    now.t = t_new
+    return now()
+
+#######################################################################################################################
+# Event Template
 
 @dataclass
 class Event:
+    """Object used to template events in a mission ConOps"""
     name: str
-
-@dataclass 
-class EventInstance:
-    name: str
-    parent: Event
-    time: float
 
 #######################################################################################################################
 # Activities
@@ -35,30 +54,70 @@ class Vehicle:
     propload: float
     trace: pd.DataFrame = pd.DataFrame(columns=['CurrentEvent', 'NextEvent', 'Prop', 'Activity'])
 
-def start_vehicle(name, conops, propload = 100):
-    v = Vehicle(name, conops.first(), propload)
-    v.trace = pd.concat([
-        v.trace,
-        pd.DataFrame({"CurrentEvent": v.activity.start, "NextEvent": v.activity.end, "Prop": v.propload, "Activity": v.activity}, index = [len(v.trace) + 1])
-    ])
-    return v
+    def __repr__(self):
+        return (f'{self.__class__.__name__} - {self.name}')
+
+    @staticmethod
+    def initialize(name, conops, propload = 100):
+        v = Vehicle(name, conops.first(), propload)
+        v.update_trace()
+        return v
+
+    def schedule_next_event(self, future_event_list):
+        template = self.activity.end
+        name = self.activity.end.name
+        heappush(
+            future_event_list.events,
+            ScheduledEvent(name, template, self, now() + self.activity.duration)
+        )
+
+    def update_trace(self):
+        self.trace = pd.concat([
+            self.trace,
+            pd.DataFrame({
+                "CurrentEvent": self.activity.start, 
+                "NextEvent": self.activity.end, 
+                "Prop": self.propload, 
+                "Activity": self.activity}, index = [len(self.trace) + 1])
+        ])
+
+#######################################################################################################################
+# Future Events
+
+@dataclass(order=True)
+class ScheduledEvent:
+    name: str=field(compare=False)
+    template: Event=field(compare=False)
+    vehicle: Vehicle=field(compare=False)
+    time: float
+
+# Implement the FutureEventList from Prof. Vuduc's example
+class FutureEventList:
+    def __init__(self):
+        self.events = []
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self) -> Event:
+        from heapq import heappop
+        if self.events:
+            return heappop(self.events)
+        raise StopIteration
+
+    def __repr__(self) -> str:
+        from pprint import pformat
+        return pformat(self.events)
+
+    def get_next(self):
+        return heappop(self.events)
 
 #######################################################################################################################
 # ConOps
 
-INIT = Event("INIT")
-liftoff = Event("Liftoff")
-stage = Event("Stage")
-orbit = Event("Orbit")
-
-# @dataclass
+@dataclass
 class ConOps:
-
-    sequence = {
-        INIT.name: Activity("waiting", INIT, liftoff, 0),
-        liftoff.name: Activity("s1_ascent", liftoff, stage, 10),
-        stage.name: Activity("s2_ascent", stage, orbit, 10)
-    }
+    sequence: dict
 
     def first(self):
         return self.sequence["INIT"]
@@ -67,60 +126,103 @@ class ConOps:
         # Get the activity which starts with a particular event
         return self.sequence[current_event.name]
 
-
-def schedule(activity, future_events):
-    end = activity.end
-    future_events.append(
-        EventInstance(end.name, end, CLOCK + v.activity.duration)
-    )
-
 #######################################################################################################################
 # Demo
 #######################################################################################################################
 
 if __name__ == "__main__":
-    CLOCK = 0
-    future_events = []
 
-    conops = ConOps()
+    # -----------------------------------------------------------------------------------------------------------------
+    # Mission Definition
+    # - Events
+    INIT = Event("INIT")
+    liftoff = Event("Liftoff")
+    stage = Event("Stage")
+    orbit = Event("Orbit")
 
-    # Start with some intial state
-    v = start_vehicle("LV1", conops)
+    DONE = Event("Done")
 
-    # Vehicle starts in some activity, which will end when that activities event is processed
-    current_activity = v.activity
-    schedule(v.activity, future_events)
+    # - Activities
+    activities = {
+        INIT.name: Activity("waiting", INIT, liftoff, 0),
+        liftoff.name: Activity("s1_ascent", liftoff, stage, 10),
+        stage.name: Activity("s2_ascent", stage, orbit, 10),
+        orbit.name: Activity("insertion", orbit, DONE, 5)
+    }
 
-    # Trigger the ending event
-    current_event = future_events[0]
-    future_events.remove(current_event)
+    conops = ConOps(activities)
 
-    # Process the event
-    # - Update the system state
-    # - Update the vehicle state
-    # v.propload -= 60  # some fancy code to handle this
+    # -----------------------------------------------------------------------------------------------------------------
+    # Initialization
+    # - Set up the system state
+    system_state = {
+        activity.name: [] for activity in conops.sequence.values()
+    }
 
-    # Get the next activity
-    next_activity = conops.after(current_event)
+    # - Reset the clock
+    set_time(0)
 
-    print(f"The current activity is {current_activity}")
-    print(f"The current event is {current_event}")
-    print(f"The next activity is {next_activity}")
+    # - Create and empty events list
+    future = FutureEventList()
 
-    # - Change the activity
-    v.activity = next_activity
+    # - Start a vehicle
+    current_vehicle = Vehicle.initialize("LV1", conops)
 
-    # Schedule the next event
-    schedule(v.activity, future_events)
-    future_events
+    # - Schedule the first event expected for the vehicle
+    current_vehicle.schedule_next_event(future)
 
-    assert current_event.parent == next_activity.start
+    # Add the vehicle to the empty system state
+    system_state[current_vehicle.activity.name].append(current_vehicle)
+    # system_state  # update the system state trace here
 
-    # - Update the vehicle trace
-    v.trace = pd.concat([
-        v.trace,
-        pd.DataFrame({"CurrentEvent": v.activity.start, "NextEvent": v.activity.end, "Prop": v.propload, "Activity": v.activity}, index = [len(v.trace) + 1])
-    ])
+    # -----------------------------------------------------------------------------------------------------------------
+    # Simulation Loop
+    print("\n\n********************\n* BEGIN SIMULATION *\n********************\n")
 
-    v.trace
+    for event in future:
 
+        # Update the simulation clock
+        set_time(event.time)
+
+        if event.template is DONE:
+            system_state[event.vehicle.activity.name].remove(vehicle)
+            print(f"Time is {now()}")
+            print("\n******************\n* END SIMULATION *\n******************")
+            break
+
+        # Transfer control to the vehicle process
+        vehicle = event.vehicle
+
+        # Process the event
+
+        # - Update the vehicle state
+        vehicle.propload -= 60  #TODO: Add proper logic for propellant updates
+
+        # Get the next activity & update the vehilce
+        previous_activity = vehicle.activity
+        vehicle.activity = conops.after(event)
+
+        # - Update the vehicle trace
+        vehicle.update_trace()
+
+        print(f"Time is {now()}")
+        print(f"\tThe previous activity was {previous_activity.name}")
+        print(f"\tThe current event is {event.name}")
+        print(f"\tThe next activity is {vehicle.activity.name}\n")
+
+        # Update the system state
+        system_state[previous_activity.name].remove(vehicle)
+        system_state[vehicle.activity.name].append(vehicle)
+
+        # Schedule the next event
+        vehicle.schedule_next_event(future)
+
+        # Return control to the scheduler
+
+    # -----------------------------------------------------------------------------------------------------------------
+    # Post-sim Report
+    print("\nVehicle Trace:")
+    print(vehicle.trace)
+
+    print("\nSystem State:")
+    print(system_state)
